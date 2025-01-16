5a0118e7f2e78ce4bcd68ad8e690205e4d56322e
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-01-16 15:18:19 +0800

    add MenuSection to Dashboard for improved navigation

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
+import MenuSection from "./dasboardSections/MenuSection";
 import ProfileSection from "./dasboardSections/ProfileSection";
 
 const Dashboard = () => {
   return (
     <div className="p-6 md:p-10 lg:p-12">
       <ProfileSection />
+      <MenuSection />
     </div>
   );
 };
