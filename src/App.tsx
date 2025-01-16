52e067c622bf050277aef4db8d2310f1055e0515
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-01-16 11:59:34 +0800

    refactor Header and Footer components to use DaisyUI for improved navigation and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import Dashboard from './pages/Dashboard';
-const appName = import.meta.env.VITE_APP_NAME || 'SISFO';
 
 const App = () => {
   return (
     <div className="App flex flex-col min-h-screen">
-      <header className="App-header">
-      <h1>{appName}</h1>
-      </header>
       <Dashboard/>
     </div>
   );
