8c7c7bd8dd434d147f9690321cbd22903e5a401e
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-01-16 12:19:21 +0800

    refactor App and Dashboard components to streamline structure and remove dark mode logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // Menyimpan preferensi tema di localStorage dan memastikan tema diterapkan pada saat pertama kali dimuat
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-      setIsDarkMode(true);
-      document.documentElement.classList.add('dark'); // Aktifkan dark mode
-    } else {
-      setIsDarkMode(false);
-      document.documentElement.classList.remove('dark'); // Nonaktifkan dark mode
-    }
-  }, []);
-
-  // Fungsi untuk toggle dark mode
-  const toggleTheme = () => {
-    setIsDarkMode((prevState) => {
-      const newState = !prevState;
-      localStorage.setItem('theme', newState ? 'dark' : 'light');
-      if (newState) {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-      return newState;
-    });
-  };
-
   return (
     <div className="App flex flex-col min-h-screen">
-      <button 
-        onClick={toggleTheme} 
-        className="p-2 m-4 bg-blue-500 text-white rounded">
-        Toggle to {isDarkMode ? 'Light' : 'Dark'} Mode
-      </button>
-      <Dashboard />
+           <Header />
+      <Dashboard/>
+      <Footer/>
     </div>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import ThemeController from '../components/ThemeController';
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text">
-      <Header />
-
       <div className="container mx-auto p-4">
         <h2 className="text-2xl font-semibold mb-4">Selamat datang di Dashboard!</h2>
         <p className="mb-4">Ini adalah halaman dashboard untuk sistem informasi akademik.</p>
         <p>Data dan informasi akademik kamu bisa dilihat di sini.</p>
       </div>
-
-      <Footer />
       <ThemeController />
     </div>
   );
