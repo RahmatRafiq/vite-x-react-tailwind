328fb4c2f1a2185f20e524d2e0302aa3d23927e4
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-01-16 14:53:40 +0800

    add ProfileSection component to Dashboard for enhanced user engagement

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
+import ProfileSection from "./dasboardSections/ProfileSection";
 
 const Dashboard = () => {
   return (
-    <div className="min-h-screen bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text">
-      <div className="container mx-auto p-4">
-        <h2 className="text-2xl font-semibold mb-4">Selamat datang di Dashboard!</h2>
-        <p className="mb-4">Ini adalah halaman dashboard untuk sistem informasi akademik.</p>
-        <p>Data dan informasi akademik kamu bisa dilihat di sini.</p>
-      </div>
+    <div className="p-6 md:p-10 lg:p-12">
+      <ProfileSection />
     </div>
   );
 };
