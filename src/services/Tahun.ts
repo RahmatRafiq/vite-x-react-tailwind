98ca8f662e2e7f3ab9e9a2cfbecb8974c4ed143d
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-18 22:07:37 +0800

    add getTahunKhsMahasiswa service and remove duplicate implementation from TahunKhs

---
+++ src/services/Tahun.ts
@@ -0,0 +1,7 @@
+import { TahunKHSResponse } from "@/types/tahun";
+import fetchData from "./FetchData";
+
+export const getTahunKhsMahasiswa = async (): Promise<TahunKHSResponse> => {
+    const data = await fetchData<TahunKHSResponse>(`${import.meta.env.VITE_APP_API_URL}/nilai/tahun_khs`);
+    return data || { status: "error", data: [], code: 500 };
+  };
