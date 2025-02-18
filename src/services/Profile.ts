8ab2f34f39827eb39f06ec98d7570f319a45ffbe
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-19 00:02:00 +0800

    feat: add getNilaiMahasiswa function to fetch student grades and remove console log from getProfilMahasiswa

--- src/services/Profile.ts
+++ src/services/Profile.ts
@@ -5,7 +5,6 @@ import fetchData from "./FetchData";
 
 export const getProfilMahasiswa = async (): Promise<ProfileResponse | null> => {
     const data = await fetchData<ProfileResponse>(`${import.meta.env.VITE_APP_API_URL}/profil-singkat/`);
-    console.log(data);
     return data || { status: "error", data: [], code: 500 };
 };
 
