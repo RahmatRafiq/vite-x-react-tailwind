6d2b8f62d9a5e66de32cb60d6689b5e85b214924
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-22 09:41:36 +0800

    feat: add capacitor build and settings configuration files with updated app ID and name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'sisfo-mobile',
+  appId: 'com.sisfo.mobile',
+  appName: 'My Stikes',
   webDir: 'dist'
 };
 
