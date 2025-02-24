05c91853fd1ea3eb2799132c2069539c594a8990
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-24 19:48:26 +0800

    feat: update MahasiswaForm to make certain fields read-only and enhance layout

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -117,6 +117,7 @@ const MahasiswaForm = () => {
             <input
               type="text"
               name="nama"
+              readOnly
               value={mahasiswa.nama}
               onChange={handleChangeMahasiswa}
               placeholder="Nama Mahasiswa"
@@ -124,42 +125,14 @@ const MahasiswaForm = () => {
               required
             />
           </label>
-
           <label className="form-control w-full sm:max-w-md">
             <div className="label">
-              <span className="label-text">Alamat Mahasiswa</span>
-            </div>
-            <input
-              type="text"
-              name="alamat"
-              value={mahasiswa.alamat}
-              onChange={handleChangeMahasiswa}
-              placeholder="Alamat Mahasiswa"
-              className="input input-bordered w-full"
-            />
-          </label>
-
-          <label className="form-control w-full sm:max-w-md">
-            <div className="label">
-              <span className="label-text">Program ID</span>
-            </div>
-            <input
-              type="text"
-              name="program_id"
-              value={mahasiswa.program_id}
-              onChange={handleChangeMahasiswa}
-              placeholder="Program ID"
-              className="input input-bordered w-full"
-            />
-          </label>
-
-          <label className="form-control w-full sm:max-w-md">
-            <div className="label">
-              <span className="label-text">Prodi ID</span>
+              <span className="label-text">Program Studi</span>
             </div>
             <input
               type="text"
               name="prodi_id"
+              readOnly
               value={mahasiswa.prodi_id}
               onChange={handleChangeMahasiswa}
               placeholder="Prodi ID"
@@ -174,6 +147,7 @@ const MahasiswaForm = () => {
             <input
               type="email"
               name="email"
+              readOnly
               value={mahasiswa.email}
               onChange={handleChangeMahasiswa}
               placeholder="Email Mahasiswa"
@@ -181,7 +155,19 @@ const MahasiswaForm = () => {
               required
             />
           </label>
-
+          <label className="form-control w-full sm:max-w-md">
+            <div className="label">
+              <span className="label-text">Alamat</span>
+            </div>
+            <input
+              type="text"
+              name="alamat"
+              value={mahasiswa.alamat}
+              onChange={handleChangeMahasiswa}
+              placeholder="Alamat Mahasiswa"
+              className="input input-bordered w-full"
+            />
+          </label>
           <label className="form-control w-full sm:max-w-md">
             <div className="label">
               <span className="label-text">Handphone</span>
@@ -201,9 +187,9 @@ const MahasiswaForm = () => {
           </button>
 
           {modalOpen && (
-            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-              <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm">
-                <h3 className="text-lg font-bold mb-4">Konfirmasi</h3>
+            <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm">
+              <div className="bg-base-100 p-6 rounded-lg shadow-lg max-w-sm border border-gray-300">
+                <h3 className="text-lg text-base-content font-bold mb-4">Konfirmasi</h3>
                 <p>Apakah Anda yakin ingin menyimpan data ini?</p>
                 <div className="flex justify-end mt-4 space-x-2">
                   <button className="btn btn-secondary" onClick={() => setModalOpen(false)}>
