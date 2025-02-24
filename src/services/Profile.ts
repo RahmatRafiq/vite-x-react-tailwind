a0af73a9a54a004c2307468ad2c682b017447749
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-24 19:15:48 +0800

    feat: add MahasiswaUpsert service for updating and retrieving student profiles

--- src/services/Profile.ts
+++ src/services/Profile.ts
@@ -4,8 +4,17 @@ import fetchData from "./FetchData";
 
 
 export const getProfilMahasiswa = async (): Promise<ProfileResponse | null> => {
-    const data = await fetchData<ProfileResponse>(`${import.meta.env.VITE_APP_API_URL}/profil-singkat/`);
+    const data = await fetchData<ProfileResponse>(`${import.meta.env.VITE_APP_API_URL}/profil/profil-singkat`);
     return data || { status: "error", data: [], code: 500 };
 };
 
 
+export const updateProfilMahasiswa = async (mhsw_id: string, mahasiswa: string): Promise<ProfileResponse> => {
+    return await fetchData<ProfileResponse>(
+        `${import.meta.env.VITE_APP_API_URL}/profil/update/${mhsw_id}`,
+        {
+            method: "POST",
+            data: mahasiswa,
+        }
+    ) || ({ status: "error", data: [], code: 500 } as unknown as ProfileResponse);
+}
