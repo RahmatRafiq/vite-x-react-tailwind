873da3339fe087e760944c47a6b5f43299034d1d
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-26 20:11:03 +0800

    feat: add KrsPage component for displaying grades

---
+++ src/pages/KrsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const KrsPage = () => {
+    return (
+        <div className="p-6 md:p-10 lg:p-12 space-y-4">
+            <h1>Nilai</h1>
+        </div>
+    );
+};
+
+export default KrsPage;
