14490e68aba26b98de6773eb786c1e8036587e55
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-02-26 23:21:38 +0800

    feat: refactor KrsPage to PaketPage and implement PaketSection for course display

--- src/pages/KrsPage.tsx
+++ src/pages/KrsPage.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
-import KrsSection from './KrsSections/KrsSections';
 import ProfileSection from './dasboardSections/ProfileSection';
+import PaketSection from './KrsSections/KrsSections';
 
+const dummyPaketData = [
+  {
+    mk_id: 943,
+    mk_kode: "KEP23702",
+    nama_mk: "BIOSTATISTIK",
+    sks: 2,
+    dosen: "SRI WAHYUNI BAHRUM",
+  },
+  {
+    mk_paket_isi_id: 966,
+    mk_id: 943,
+    mk_kode: "KEP23702",
+    nama_mk: "BIOSTATISTIK",
+    sks: 2,
+    dosen: "SRI WAHYUNI BAHRUM",
+  },
+  {
+    mk_id: 943,
+    mk_kode: "KEP23702",
+    nama_mk: "BIOSTATISTIK",
+    sks: 2,
+    dosen: "SRI WAHYUNI BAHRUM",
+  },
+  {
+    mk_id: 943,
+    mk_kode: "KEP23702",
+    nama_mk: "BIOSTATISTIK",
+    sks: 2,
+    dosen: "SRI WAHYUNI BAHRUM",
+  },
+  {
+    mk_id: 943,
+    mk_kode: "KEP23702",
+    nama_mk: "BIOSTATISTIK",
+    sks: 2,
+    dosen: "SRI WAHYUNI BAHRUM",
+  }
+];
 
-const KrsPage = () => {
-    return (
-        <div className="p-6 md:p-10 lg:p-12 space-y-4">
-             <ProfileSection />
-            <KrsSection />
-        </div>
-    );
+const PaketPage = () => {
+  return (
+    <div className="p-6 md:p-10 lg:p-12 space-y-4">
+      <ProfileSection />
+      <PaketSection mataKuliahPaket={dummyPaketData} />
+    </div>
+  );
 };
 
-export default KrsPage;
+export default PaketPage;
--- src/pages/KrsSections/KrsSections.tsx
+++ src/pages/KrsSections/KrsSections.tsx
@@ -1,9 +1,38 @@
-const KrsSection = () => {
-    return (
-        <div className="p-6 md:p-10 lg:p-12 space-y-4">
-            <h1>OKE</h1>
-        </div>
-    );
+import React from 'react';
+
+interface MataKuliahPaket {
+  mk_id: number;
+  nama_mk: string;
+  sks: number;
+  dosen: string;
+}
+
+const PaketSection = ({ mataKuliahPaket }: { mataKuliahPaket: MataKuliahPaket[] }) => {
+  return (
+    <div className=" card bg-base-300 p-4 rounded-lg shadow-md mt-4">
+      <h2 className="text-xl font-semibold mb-4">Daftar Mata Kuliah Semester Depan</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th>Nama Mata Kuliah</th>
+              <th>SKS</th>
+              <th>Dosen</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mataKuliahPaket.map((mk) => (
+                <tr key={mk.mk_id}>
+                <td>{mk.nama_mk}</td>
+                <td>{mk.sks}</td>
+                <td>{mk.dosen}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
-export default KrsSection;
+export default PaketSection;
